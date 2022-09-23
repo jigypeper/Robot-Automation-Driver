c473e5b2b3e6da2662ed160a94d9fbed2f37839f
ahmed <ahmedosman@mac.com>
2022-09-23 09:19:00 +0100

    refactored

--- test_driver.py
+++ test_driver.py
@@ -1,27 +1,27 @@
 from driver import DeviceDriver
 
-robot = DeviceDriver()
+driver = DeviceDriver()
 
 
 def test_execute():
-    result = robot.execute_operation("apple", ["place"], [60])
+    result = driver.execute_operation("apple", ["place"], [60])
 
     assert result == "Not a valid operation."
 
 
 def test_pick():
-    result = robot.execute_operation("pick", ["Destination"], [80])
+    result = driver.execute_operation("pick", ["Destination"], [80])
 
     assert result == "Wrong combination of parameters"
 
 
 def test_place():
-    result = robot.execute_operation("place", ["Source"], [80])
+    result = driver.execute_operation("place", ["Source"], [80])
 
     assert result == "Wrong combination of parameters"
 
 
 def test_transfer():
-    result = robot.execute_operation("transfer", ["origin", "apple"], [80, 100])
+    result = driver.execute_operation("transfer", ["origin", "apple"], [80, 100])
 
     assert result == "Wrong combination of parameters"
