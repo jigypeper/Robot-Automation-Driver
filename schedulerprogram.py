11203fd0f40a285213869cdc3cac5504485bba1b
ahmed <ahmedosman@mac.com>
2022-09-23 09:35:34 +0100

    comments added

--- schedulerprogram.py
+++ schedulerprogram.py
@@ -1,18 +1,25 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ip = '127.0.0.1'
+ip = "localhost"  # '127.0.0.1'
 port = 4500
 address = (ip, port)
 run = True
 while run:
+    # connect to driver
     client = socket.socket()
     client.connect(address)
-    message = input("> ")
-    client.sendall(message.encode('utf-8'))
 
+    # ask for operation
+    message = input("> ")
+
+    # send operation
+    client.sendall(message.encode("utf-8"))
+
+    # stop loop if abort instruction sent
     if "abort" in message:
         run = False
 
-    data = client.recv(1024).decode('utf-8')
+    # receive data from driver
+    data = client.recv(1024).decode("utf-8")
     print(data)
