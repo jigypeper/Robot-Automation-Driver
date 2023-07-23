5a69198ff5e2bf8fda373d5682328a78db3da077
Ahmed Hagi <ahmedosman@mac.com>
2023-07-23 22:31:38 +0100

    Update mockrobot.py
    More type hints

--- mockrobot.py
+++ mockrobot.py
@@ -11,22 +11,22 @@ class MockRobot:
         MockRobot.processID = ""
         MockRobot.processStatus = ""
 
-    def home(self):
+    def home(self) -> int:
         sleep(random.randint(0, 2))
         MockRobot.processID = random.randint(1, 1000)
         return MockRobot.processID
 
-    def pick(self, sourceLocation: int):
+    def pick(self, sourceLocation: int) -> int:
         sleep(random.randint(0, 5))
         MockRobot.processID = sourceLocation + 1
         return MockRobot.processID
         
-    def place(self, destinationLocation: int):
+    def place(self, destinationLocation: int) -> int:
         sleep(random.randint(0, 5))
         MockRobot.processID = destinationLocation + 1
         return MockRobot.processID
         
-    def status(self):
+    def status(self) -> str:
         MockRobot.processStatus = random.choice(["In Progress", "Finished Successfully", "Terminated With Error"])
         return MockRobot.processStatus
         
